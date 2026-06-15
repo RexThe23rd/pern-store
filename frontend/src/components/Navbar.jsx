9d36869cc0db6a171922af497b3454aa4162c142
RexThe23rd <rexthe23rd@gmail.com>
2026-06-15 13:01:33 -0500

    progressed in the nav bar, conditional rendering for the cart. General bugfixing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Link, useResolvedPath } from "react-router-dom"
+import { Code, ShoppingCartIcon } from "lucide-react"
 
 function Navbar() {
+  const {pathname} = useResolvedPath()
+  const isHomePage = pathname === "/"
+
   return (
-    <div>Navbar</div>
+    // icon & title (logo)
+    <div className = "bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+      <div className='max-w-7xl mx-auto'>
+        <div className="navbar px-4 min-h-16 justify-between">
+          <div className="flex-2 items-center gap-2">
+            <Link to="/" className="hover-opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <Code className="size-9 text-primary" />
+                <span className="font-semibold font-mono tracking-widest text-2xl bg-clip-text text-transparent bg-linear-to-r from-primary to-secondary">
+                  PERN STORE
+                </span>
+              </div>
+            </Link>
+          </div>
+          {/* right side (nav links) */}
+          <div className="flex items-center gap-4">
+            <p>ThemeSelector</p>
+
+            {isHomePage && (
+              <div className="indicator">
+                <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
+                  <ShoppingCartIcon className="size-5" />
+                  <span className="badge badge-sm badge-primary indicator-item">3</span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 
