8372bd1244512c1b85f0d05e6b8b9c5937f7ba48
RexThe23rd <rexthe23rd@gmail.com>
2026-06-29 15:35:32 -0500

    fixed some dependencies and finished deployment setup

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,17 @@ import productRoutes from './routes/productRoutes.js';
 import { sql } from './config/db.js';
 import { aj } from './lib/arcjet.js';
 import e from 'express';
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false, }));
 app.use(morgan('dev'));
 
 app.use(async (req, res, next) => {
@@ -34,7 +36,7 @@ app.use(async (req, res, next) => {
             }
             return;
         }
-        
+
         if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
             res.status(403).json({ error: "Spoofed bot access denied." });
             return;
@@ -48,6 +50,13 @@ app.use(async (req, res, next) => {
 })
 
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("/{*splat}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 async function initDB() {
     try {
